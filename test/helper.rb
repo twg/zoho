be840c124339e27ebbb145dcbbe62caaa88afecc
dexx1220 <dexter@theworkinggroup.ca>
2015-04-16 10:48:19 -0400

    adding coveralls gem

--- test/helper.rb
+++ test/helper.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'minitest/autorun'
 require 'fileutils'
 require 'vcr'
+require 'coveralls'
+
+Coveralls.wear! 'rails'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
